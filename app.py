41675d472ebbcbbbc002dd22c2cb7c8b0c036acf
UPTM-ultimo-anio <javieraugustoparedes@gmail.com>
2021-02-06 23:33:43 -0400

    Added PostgreSQL enviroment variable and psycopg2.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ Created on Sat Nov  7 13:04:11 2020
 
 @author: Javier Paredes
 """
+import os
 
 from flask import Flask, request 
 from flask_restful import Resource, Api, reqparse
@@ -16,7 +17,7 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
